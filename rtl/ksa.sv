9a3754e45b02fbddd345ef4d2b881f88ddcd5ac3
Eldad Zipori <eldad.z.zipori@gmail.com>
2024-06-12 16:39:48 -0700

    Added ROM control [doesnt work yet]

--- rtl/ksa.sv
+++ rtl/ksa.sv
@@ -91,6 +91,32 @@ module ksa
 		 .sij_ready				(shuffle_mem_s_i_j_avail),
 		 .shuffle_finished	(shuffle_mem_finished)
 	); */
+	
+	/*
+		Reading from ROM memory
+	*/
+	/*
+	logic[7:0] 	rom_data[31:0];
+	logic[7:0] 	rom_q_data_out;
+	logic[5:0]	rom_reader_address_out;
+	logic			rom_reader_done;
+	
+	encrypted_data_memory rom_memory(
+		.address	(rom_reader_address_out),
+		.clock	(CLOCK_50 & (!rom_reader_done)),			// When rom_read_done flag is up stop reading
+		.q			(rom_q_data_out)
+	);
+	
+	read_rom_mem(
+		.clk				(CLOCK_50),
+		.reset			(1'b0),
+		.rom_q_data_in	(rom_q_data_out),	
+		.done				(rom_reader_done),
+		.address			(rom_reader_address_out),
+		.rom_data		(rom_data)
+	);
+
+	*/
 	/*
 		MUX to control which signals conrtol the S memory
 		[*] Move to module at the end
