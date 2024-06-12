b12484f84bf69691bce67486dacb5a172c5e1583
Eldad Zipori <eldad.z.zipori@gmail.com>
2024-06-12 15:16:18 -0700

    add fundation for task 2

--- rtl/ksa.sv
+++ rtl/ksa.sv
@@ -5,8 +5,10 @@
 
 module ksa
 (
-	input logic CLOCK_50
-
+	input logic CLOCK_50,
+	input  logic[9:0] SW,
+	
+	output logic[9:0] LEDR
 );
 
 	/*
@@ -27,7 +29,7 @@ module ksa
 	
 	
 	/*
-		Task 1 
+		TASK 1 
 		Populating the S memory location by the address
 	*/
 	logic	[7:0]	by_index_address_out;
@@ -44,7 +46,50 @@ module ksa
 		
 	);
 	
+	/*
+		TASK 2
+	*/
 	
+	/*
+		Switch controls
+	*/
+	logic [23:0] secret_key;
+	logic key_from_switches_available;
+	logic key_from_switches_changed;
+	
+	switches_fsm key_switches_control(
+		.CLOCK_50		(CLOCK_50),
+		.reset			(1'b0),
+		.SW				(SW),
+		.LEDR				(LEDR),
+		.secret_key		(secret_key[9:0]),
+		.key_available (key_from_switches_available),
+		.key_changed	(key_from_switches_changed)	
+	);
+	
+	/*
+		Shuffle memory control
+	*/
+	logic [8:0] shuffle_mem_index;
+	logic	[7:0]	shuffle_mem_data_out;
+	logic [7:0]	shuffle_mem_address_out;
+	logic 		shuffle_mem_s_i_j_avail;
+	logic			shuffle_mem_finished;
+	logic			shuffle_mem_write_enable;
+	
+	shuffle_fsm shuffle_control # (.KEY_LENGTH(3))
+	(    
+		 .CLOCK_50				(CLOCK_50),
+		 .reset					(1'b0),
+		 .secret_key			(secret_key),
+		 .s						(s_memory_q_data_out),
+		 .index					(shuffle_mem_index),
+		 .write_enble			(shuffle_mem_write_enable)
+		 .data					(shuffle_mem_data_out),
+		 .address				(shuffle_mem_address_out),
+		 .sij_ready				(shuffle_mem_s_i_j_avail),
+		 .shuffle_finished	(shuffle_mem_finished)
+	);
 	/*
 		MUX to control which signals conrtol the S memory
 		[*] Move to module at the end
@@ -54,6 +99,11 @@ module ksa
 			s_memory_address_in	=	by_index_address_out;
 			s_memory_data_in		=	by_index_data_out;
 			s_memory_data_enable	=	by_index_data_enable;
+		end
+		else if (!shuffle_mem_finished) begin
+			s_memory_data_enable = 	shuffle_mem_write_enable;
+			s_memory_data_in		=	shuffle_mem_data_out;
+			s_memory_address_in	= 	shuffle_mem_address_out;
 		end
 		else begin
 			s_memory_address_in	=	0;
