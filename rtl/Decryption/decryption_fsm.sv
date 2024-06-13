6722ec163b6da54de73431c058bb145dff2af10f
Eldad Zipori <eldad.z.zipori@gmail.com>
2024-06-12 22:20:41 -0700

    fixing syntesis

--- rtl/Decryption/decryption_fsm.sv
+++ rtl/Decryption/decryption_fsm.sv
@@ -2,20 +2,15 @@
 
 
 module decryption_fsm
-# (parameter MSG_DEP = 32, parameter MSG_WIDTH = 8)
+# (parameter MSG_DEP = 32, parameter S_DEP = 256, parameter MSG_WIDTH = 8)
 (
 	input logic							clk,
 	input logic							reset,
 	input logic [MSG_WIDTH-1:0]	encrypted_input[MSG_DEP-1:0],
-	input logic [MSG_WIDTH-1:0] 	s_i,
-	input	logic	[MSG_WIDTH-1:0]	s_j,
-	input logic							s_j_available,
-	input logic [MSG_WIDTH-1:0]	s_data_in,
+	input logic [MSG_WIDTH-1:0]	s_data[S_DEP-1:0],
 	
-	output logic [MSG_WIDTH-1:0]	decrypted_output[MSG_DEP-1:0],
-	output logic [MSG_WIDTH:0]		current_index,
-	
-	output logic [MSG-WIDTH-1:0] 	address_out,
+	output logic [MSG_WIDTH-1:0]	decrypted_output[MSG_DEP-1:0],	
+	output logic [MSG_WIDTH-1:0] 	address_out,
 	output logic						wn_en,
 	output logic						done
 );
@@ -28,21 +23,20 @@ module decryption_fsm
 		3			available flag
 		4			counter_finished flag
 	*/
-	localparam IDLE 				= 0;
-	localparam WAIT_AVAIL		= 1;
-	localparam SEND_S_ADDR		= 10;
-	localparam WAIT_S_DATA		= 11;
-	localparam ASSIGN_F			= 100;
-	localparam DECRYPT			= 101;
-	localparam INCREMENT_INDEX	= 110;
-	localparam DETERMINE			= 111;
-	localparam DONE				= 1000;
+	localparam IDLE 					= 0;
+	localparam ASSIGN_F				= 001;
+	localparam DECRYPT				= 010;
+	localparam INCREMENT_INDEX_I	= 011;
+	localparam INCREMENT_INDEX_J	= 100;
+	localparam DETERMINE				= 101;
+	localparam DONE					= 110;
 	
 	
 	logic [4:0] current_state/*synthesis keep*/;
 	logic [4:0] next_state;
 	
 	logic [MSG_WIDTH-1:0] 	f;	
+	logic	[7:0]					index_i, index_j;
 	
 	// Flip flop to register the current state
 	always_ff @(posedge clk) begin
@@ -50,38 +44,32 @@ module decryption_fsm
 	end
 	
 	always_comb begin
-		if(reset) next_state <= IDLE
+		if(reset) next_state <= IDLE;
 		else begin
 			case (current_state)
 				IDLE: begin
-					next_state <= WAIT_AVAIL;
+					next_state <= INCREMENT_INDEX_I;
 				end
-				WAIT_AVAIL: begin
-					if(s_j_available)	next_state = SEND_S_ADDR; 	// Only register s[i] and s[j] when they are available
-					else 					next_state = IDLE;
+				INCREMENT_INDEX_I: begin
+					next_state = INCREMENT_INDEX_J;
 				end
-				SEND_S_ADDR: begin
-					next_state = WAIT_S_DATA;
-				end
-				WAIT_S_DATA: begin
-					next_state = ASSIGN_F;
+				INCREMENT_INDEX_J: begin
+					next_state = DECRYPT;
 				end
 				ASSIGN_F: begin
 					next_state = DECRYPT;
 				end
 				DECRYPT: begin
-					next_state = INCREMENT_INDEX;
-				end
-				INCREMENT_INDEX: begin
 					next_state = DETERMINE;
 				end
-				DETEMINE: begin
-					if (current_index == ({MSG_WIDTH,1'b1} + 1))  	next_state = DONE;
-					else															next_state = WAIT_AVAIL;	
+				DETERMINE: begin
+					if (index_i == ({MSG_WIDTH,1'b1} - 1))  			next_state = DONE;					// when all data is read stop 
+					else															next_state = INCREMENT_INDEX_I;	
 				end
 				DONE: begin
 					next_state = DONE;
 				end
+				default: next_state = IDLE;
 			endcase
 		end
 	end
@@ -91,20 +79,22 @@ module decryption_fsm
 	always_ff @ (posedge clk) begin
 		case (current_state)
 			IDLE: begin
-				current_index <= 0;
+				index_i 	<= 0;
+				index_j	<= 0;
 			end
-			SEND_S_ADDR: begin
-				address_out <= s_i + s_j;
+			INCREMENT_INDEX_I: begin
+				index_i <= index_i + 1;
+			end
+			INCREMENT_INDEX_J: begin
+				index_j <= index_j + s_data[index_i];
 			end
 			ASSIGN_F: begin
-				f <= s_data_in;
+				f <= s_data[s_data[index_i] +s_data[index_j]];
 			end
 			DECRYPT: begin
-				decrypted_output[current_index] = f ^ encrypted_input[current_i];
+				decrypted_output[index_i] = f ^ encrypted_input[index_i];
 			end
-			INCREMENT_INDEX: begin
-				current_index <= current_index + 1;
-			end
+
 		endcase
 	end
 	
