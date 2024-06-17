59ede8411120aad9eedfb801fa087d0a719ab652
Eldad Zipori <eldad.z.zipori@gmail.com>
2024-06-17 09:34:39 -0700

    initial added multiple cores

--- rtl/ksa.sv
+++ rtl/ksa.sv
@@ -21,180 +21,117 @@ module ksa
 	
 	assign reset = SW[0];
 	
-	localparam IDLE 					= 0;
-	localparam START_READ_ROM_D 	= 1;
-	localparam READ_ROM_D			= 2;
-	localparam START_GEN_KEY		= 3;
-	localparam GEN_KEY				= 4;
-	localparam DECRYPT				= 5;
-	localparam DETERMINE				= 6;
-	localparam FOUND					= 7;
-	localparam NOT_FOUND				= 8;
-	localparam RESET					= 9;
+	localparam IDLE 				= 0;
+	localparam READ_ROM 			= 1;
+	localparam START_CORES 		= 2;
+	localparam WAIT_FOR_FIND 	= 3;
+	localparam FOUND				= 4;
+	localparam NOT_FOUND			= 5;
 	
-	logic [5:0] current_state, next_state;
+	logic read_rom_start, read_rom_done, start_cores, stop_cores;
+	logic [3:0] key_found;
+	logic [3:0] core_done;
+	
+	logic[7:0] 	decrypted_data[3:0][31:0];
+	logic[23:0] secret_key [3:0];
+	logic[2:0] 	found_index;
+	
+	logic [4:0] current_state, next_state;
 	
 	always_ff @(posedge CLOCK_50) begin
 		current_state <= next_state;
 	end
 	
-	logic read_rom_start, read_rom_done, start_determine;
-	
 	always_comb begin
-		if (reset) begin
-			next_state = IDLE;
-		end
+		if(reset) next_state = IDLE;
 		else begin
-			case(current_state)
-				IDLE: next_state = START_READ_ROM_D;
-				START_READ_ROM_D: begin
-						next_state = READ_ROM_D;
+			case (current_state)
+				IDLE: begin
+					next_state = READ_ROM;
 				end
-				READ_ROM_D: begin
-					if(read_rom_done) 	next_state = START_GEN_KEY;
-					else						next_state = READ_ROM_D;
+				READ_ROM: begin
+					if (read_rom_done)	next_state = START_CORES;
+					else						next_state = READ_ROM;
 				end
-				START_GEN_KEY: begin
-					if (key_available)	next_state = GEN_KEY;
-					else						next_state = START_GEN_KEY;
+				START_CORES: begin
+					next_state = WAIT_FOR_FIND;
 				end
-				GEN_KEY: 					next_state = DECRYPT;
-				DECRYPT: begin
-					if(decryption_done) 	next_state = DETERMINE;
-					else						next_state = DECRYPT;
+				WAIT_FOR_FIND: begin
+					if (|key_found) 			next_state = FOUND;
+					else if (&core_done)		next_state = NOT_FOUND;
+					else							next_state = WAIT_FOR_FIND; 
 				end
-				DETERMINE: begin
-					if(determine_valid_finised) begin
-						if(msg_valid)			next_state = FOUND;
-						else if(out_of_keys) next_state = NOT_FOUND;
-						else 						next_state = RESET;
-					end
-					else 							next_state = DETERMINE;
-					
-				end
-				RESET: next_state = START_GEN_KEY;
-				FOUND: next_state = FOUND;
-				NOT_FOUND: begin
-					next_state = NOT_FOUND;
-				end
-				default:	next_state = IDLE;
 			endcase
 		end
 	end
-	
-	
-	logic reset_state;
 	
 	always_ff @(posedge CLOCK_50) begin
-		if(reset) begin
-			key_read 			<= 1'b0;
-			read_rom_start 	<= 1'b0;
-			LEDR 					<= 0;
-			reset_state 		<= 1'b0;
-			new_key_available <= 1'b0;
-			start_determine 	<= 1'b0;
-			write_to_DE			<= 1'b0;
+		if (reset) begin
+			read_rom_start <= 1'b0;
+			start_cores		<= 1'b0;
+			LEDR 				<= 0;
+			write_to_DE 	<= 1'b0;
+			stop_cores		<= 1'b0;
 		end
 		else begin
-			case(current_state)
-				START_READ_ROM_D: begin
-											read_rom_start <= 1'b1;
+			case (current_state) 
+				IDLE: begin
+					read_rom_start <= 1'b0;
+					start_cores		<= 1'b0;
+					stop_cores		<= 1'b0;
 				end
-				READ_ROM_D:				read_rom_start <= 1'b0;
-				START_GEN_KEY: begin
-					if(key_available) begin
-						secret_key <= {2'b00, key_counter};
-						key_read <= 1'b1;
-						 start_determine <= 1'b0;
-					end
-					
-					
-					reset_state <= 1'b0;
+				READ_ROM: begin
+					read_rom_start <= 1'b1;
 				end
-				GEN_KEY: key_read <= 1'b0;
-				DETERMINE: start_determine <= 1'b1;
-				DECRYPT: begin
-					new_key_available <= 1'b1;
-					
-					//if(secret_key == 24'b1001001001) LEDR[5] <= 1'b1;
+				START_CORES: begin
+					read_rom_start <= 1'b0;
+					start_core		<= 1'b1;
 				end
-				NOT_FOUND: begin
-					if (out_of_keys)			LEDR[9] <= 1'b1;							
+				WAIT_FOR_FIND: begin
+					start_core 		<= 1'b0;
 				end
 				FOUND: begin
-					LEDR[0] <= 1'b1;
-					write_to_DE <= 1'b1;
+					LEDR[0] 			<= 1'b1;
+					write_to_DE		<= 1'b1;
+					stop_core		<= 1'b1;
+					case (key_found)
+						0:	found_index <= 0;
+						1:	found_index <= 1;
+						2: found_index <= 2;
+						3: found_index <= 3;
+					
+					endcase
 				end
-				RESET: reset_state <= 1'b1;
+				NOT_FOUND: begin
+					LEDR[9] 			<= 1'b1;
+				end
 			endcase
 		end
-	end
+	end	
 	
-	/* Controls the HEX display with secret_key*/
-	HEX_Control Hex_Control_inst
-	(
-		.orig_clk			(CLOCK_50),
-		.secret_key 		(secret_key),
-		.HEX0 				(HEX0),
-		.HEX1 				(HEX1),
-		.HEX2 				(HEX2),
-		.HEX3 				(HEX3),
-		.HEX4 				(HEX4),
-		.HEX5 				(HEX5)
-	);
-	
-	
-	logic [23:0] secret_key;
-	logic [21:0] key_counter;
-	logic key_available;
-	logic out_of_keys;
-	logic key_read;
-	
-	LFSR_Controller # (.OP_MODE(0))
-	key_generator
-	(
-		.clk					(CLOCK_50),
-		.reset				(1'b0),
-		.counter_read		(key_read),
-		.counter				(key_counter),
-		.available			(key_available),
-		.counter_finished	(out_of_keys)
-	);
-	
-	/*
-	logic key_from_switches_available;
-	logic key_from_switches_changed;
-	
-	switches_fsm key_switches_control(
-		.CLOCK_50		(CLOCK_50),
-		.reset			(1'b0),
-		.SW				(SW),
-		.LEDR				(LEDR),
-		.secret_key		(),
-		.key_available (key_from_switches_available),
-		.key_changed	(key_from_switches_changed)					// This sends a reset to the other state machines
-	);
-	
-	assign secret_key = {{14{1'b0}},LEDR};
-	*/
-	/*
-		RAM Memory (s) - Working Memory (256 words x 8 bit)
-	*/
-	
-	logic	[7:0]	s_memory_q_data_out;
-	
-	logic [7:0]	s_memory_address_in;
-	logic [7:0] s_memory_data_in;
-	logic			s_memory_data_enable;
-	s_memory s_memory_controller(
-		.address	(s_memory_address_in),
-		.clock	(CLOCK_50),
-		.data		(s_memory_data_in),
-		.wren		(s_memory_data_enable),				
-		.q			(s_memory_q_data_out)									
-	);
-	
-	
+	genvar i;
+	generate 
+		for (i=0; i<4; i++) begin
+			full_decryption_core
+			# (.SEED((i == 0) ? 22'h3FFFFF :
+						(i == 1) ? 22'h3FFFED :
+						(i == 2) ? 22'h3FF8 :
+						22'h30001))
+			(
+				.CLOCK_50			(CLOCK_50),
+				.reset				(reset),
+				.read_rom_done		(read_rome_done),
+				.rom_data_d			(rom_data_d),
+				.start_core			(start_core),
+				.stop_core			(stop_core),
+						
+				.secret_key			(secret_key[i]),		
+				.decrypted_data   (decrypted_data[i]),		
+				.core_done			(core_done[i]),
+				.found				(key_found[i])
+				);
+			end		
+	endgenerate
 	/*
 		ROM memory (D) - Encrypted data (32 words x 8bits)
 	*/
@@ -225,48 +162,25 @@ module ksa
 		.rom_data		(rom_reader_data_out),
 		.enable_output	(rom_reader_enable),
 	);
-
-	logic[7:0] 	decrypted_data[31:0];
-	logic decryption_done;
-	logic new_key_available;
 	
-	decryption_core decryption_core1(
-	  .clk								(CLOCK_50),
-	  .reset								(reset_state | reset),
-	  .stop								(1'b0),
-	  .s_memory_address_in			(s_memory_address_in),
-	  .s_memory_data_in				(s_memory_data_in),
-	  .s_memory_data_enable			(s_memory_data_enable),
-	  .s_memory_q_data_out			(s_memory_q_data_out),
-	  .key_from_switches_changed	(1'b0),
-	  .key_from_switches_available(1'b0),
-	  .new_key_available				(new_key_available),
-	  .ROM_mem_read					(read_rom_done),
-	  .rom_data_d						(rom_data_d),
-	  .secret_key						(secret_key),
-	  .decrypted_data					(decrypted_data),
-	  .done								(decryption_done)
-	);
-	
-	logic determine_valid_finised;
-	logic msg_valid;
-	determine_valid_message
-		# (
-		.LOW_THRESHOLD			(97),    						// ASCII value for 'a'
-		.HIGH_THRESHOLD		(122),  							// ASCII value for 'z'
-		.SPECIAL					(32),          				// ASCII value for space ' '
-		.END_INDEX				(31)        					// Last index to check (the entire message is 32)
-	)
-	validator
+	/* Controls the HEX display with secret_key*/
+	HEX_Control Hex_Control_inst
 	(
-		.CLOCK_50				(CLOCK_50),          		// Clock signal                        
-		.reset					(reset_state | reset),     // Reset signal
-		.decrypted_data		(decrypted_data),  			// Input decrypted data array
-		.decrypt_done			(start_determine), 		 	// Signal indicating decryption is done                                                                    
-		.key_valid				(msg_valid),      			// Output signal indicating if the key is valid
-		.finish					(determine_valid_finised), // Output signal indicating the checking process is finished
+		.orig_clk			(CLOCK_50),
+		.secret_key 		(secret_key),
+		.HEX0 				(HEX0),
+		.HEX1 				(HEX1),
+		.HEX2 				(HEX2),
+		.HEX3 				(HEX3),
+		.HEX4 				(HEX4),
+		.HEX5 				(HEX5)
 	);
 	
+	logic [23:0] activated_secret_key;
+	always_ff @(posedge CLOCK_50) begin
+		if (|key_found) 	activated_secret_key <= secret_key[found_index];
+		else					activated_secret_key <= secret_key[0];
+	end
 
 	logic write_to_DE;
 	logic done_writing_to_de;
@@ -277,7 +191,7 @@ module ksa
 		.clk				(CLOCK_50),
 		.reset			(1'b0),
 		.start			(write_to_DE),
-		.decrypted_data(decrypted_data),
+		.decrypted_data(decrypted_data[found_index]),
 		.done				(done_writing_to_de)
 	);
 endmodule 
